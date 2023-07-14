396b279aaf4f30e860d522a9e6b57ce638a809c5
wangkang <1226426603@qq.com>
2023-07-14 11:38:44 +0800

    feat:本地文件内存溢出处理

--- demo/src/main/java/cn/entertech/flowtimeble/ShowLogActivity.kt
+++ demo/src/main/java/cn/entertech/flowtimeble/ShowLogActivity.kt
@@ -4,39 +4,84 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.util.Log
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import java.io.BufferedReader
 import java.io.FileInputStream
 import java.io.InputStreamReader
 import kotlin.concurrent.thread
 
-class ShowLogActivity : AppCompatActivity() {
-    private lateinit var tvShowLogs:TextView
+class ShowLogActivity : AppCompatActivity(), View.OnClickListener {
+    private lateinit var tvShowLogs: TextView
+    private lateinit var btnNextLog: Button
+    private lateinit var btnPreviousLog: Button
+    private lateinit var tvIndex: TextView
     private val mainHandler by lazy {
         Handler(Looper.getMainLooper())
     }
-    private var thread:Thread?=null
+
+    private val runTime by lazy {
+        Runtime.getRuntime()
+    }
+
+    private val logCacheList: ArrayList<String> by lazy {
+        val logCacheList = ArrayList<String>()
+        logCacheList
+    }
+    private var currentIndex = -1
+    private var thread: Thread? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.show_log_activity)
-        tvShowLogs=findViewById(R.id.tvShowLogs)
+        tvShowLogs = findViewById(R.id.tvShowLogs)
+        tvIndex = findViewById(R.id.tvIndex)
+        btnNextLog = findViewById(R.id.btnNextLog)
+        btnPreviousLog = findViewById(R.id.btnPreviousLog)
+        btnPreviousLog.setOnClickListener(this)
+        btnNextLog.setOnClickListener(this)
         showLogs()
     }
 
-    private fun showLogs(){
-        thread=thread {
+    private fun showLogs() {
+        thread = thread {
             val sb = java.lang.StringBuilder()
             cacheDir.listFiles()?.forEach {
                 val rb = BufferedReader(InputStreamReader(FileInputStream(it)))
                 var str: String? = rb.readLine()?.trim()
                 while (str != null) {
                     sb.append(str)
-                    str=rb.readLine()?.trim()
+                    if (sb.length > 400000) {
+                        val firstLog = sb.toString()
+                        logCacheList.add(firstLog)
+                        sb.clear()
+                        if (currentIndex < 0) {
+                            currentIndex++
+                            tvIndex.text = currentIndex.toString()
+                            mainHandler.post {
+                                tvShowLogs.text = firstLog
+                            }
+                        }
+                        if (logCacheList.size > 100) {
+                            Log.d("wk", "大于100了 ${logCacheList.size}")
+                            break
+                        }
+                    }
+                    str = rb.readLine()?.trim()
+                }
+                val logString = sb.toString()
+                if (currentIndex > -1) {
+                    logCacheList.add(logString)
+                } else {
+                    currentIndex++
+                    tvIndex.text = currentIndex.toString()
+                    mainHandler.post {
+                        tvShowLogs.text = logString
+                    }
                 }
             }
-            mainHandler.post {
-                tvShowLogs.text=sb.toString()
-            }
+
         }
     }
 
@@ -44,5 +89,31 @@ class ShowLogActivity : AppCompatActivity() {
         thread?.interrupt()
         mainHandler.removeCallbacksAndMessages(null)
         super.onDestroy()
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.btnNextLog -> {
+                currentIndex++
+                if (currentIndex > logCacheList.size) {
+                    currentIndex = logCacheList.size - 1
+                }
+                tvIndex.text = currentIndex.toString()
+                if (currentIndex < logCacheList.size && currentIndex >= 0) {
+                    tvShowLogs.text = logCacheList[currentIndex]
+                }
+            }
+            R.id.btnPreviousLog -> {
+                currentIndex--
+                if (currentIndex < 0) {
+                    currentIndex = 0
+                }
+                tvIndex.text = currentIndex.toString()
+                if (currentIndex < logCacheList.size && currentIndex >= 0) {
+                    tvShowLogs.text = logCacheList[currentIndex]
+                }
+            }
+        }
+
     }
 }
