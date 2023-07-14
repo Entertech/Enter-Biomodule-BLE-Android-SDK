3a5bfacdc8b4037059f9c0728495148041b16708
wangkang <1226426603@qq.com>
2023-07-14 11:40:12 +0800

    feat:日志文件大小限制

--- demo/src/main/java/cn/entertech/flowtimeble/App.kt
+++ demo/src/main/java/cn/entertech/flowtimeble/App.kt
@@ -3,12 +3,11 @@ package cn.entertech.flowtimeble
 import android.app.Application
 import com.orhanobut.logger.DiskLogAdapter
 import com.orhanobut.logger.Logger
-import java.io.File
 
 class App:Application() {
     override fun onCreate() {
         super.onCreate()
-        val maxBytesSize = 5000
+        val maxBytesSize = 50 *1024*1024
         Logger.addLogAdapter(DiskLogAdapter(cacheDir, maxBytesSize))
     }
 }
