599824a3c181b8843d9ff1a03af77cf2852a9394
wangkang <1226426603@qq.com>
2023-12-22 09:50:03 +0800

    日志不保存本地

--- ble/src/main/java/cn/entertech/ble/utils/BleLogUtil.kt
+++ ble/src/main/java/cn/entertech/ble/utils/BleLogUtil.kt
@@ -9,9 +9,7 @@ import java.text.SimpleDateFormat
  * 先封装一层，使用系统日志，方便后续变更日志库
  * */
 object BleLogUtil {
-    private val sb = StringBuilder()
     private const val TAG = "BleLogUtil"
-    private const val BLANK = "    "
 
 
     fun i(msg: String) {
@@ -24,12 +22,6 @@ object BleLogUtil {
 
     fun i(tag: String, msg: String) {
         Log.i(tag, msg)
-        val current = System.currentTimeMillis()
-        val currentTime = SimpleDateFormat("yyyy-MM-dd HH:mm:ss SSS")
-        val formatTime = currentTime.format(current)
-        sb.append(formatTime).append(BLANK).append(tag).append(BLANK).appendLine(msg)
-        Logger.i(sb.toString())
-        sb.clear()
     }
 
     fun e(tag: String, msg: String) {
