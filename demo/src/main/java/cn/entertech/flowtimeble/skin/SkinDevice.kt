e7fc1be5a2c9832d5fe69972a31a82e22d19a2b5
wk <1226426603@qq.com>
2025-06-18 11:47:30 +0800

    solve build error

--- demo/src/main/java/cn/entertech/flowtimeble/skin/SkinDevice.kt
+++ demo/src/main/java/cn/entertech/flowtimeble/skin/SkinDevice.kt
@@ -2,4 +2,12 @@ package cn.entertech.flowtimeble.skin
 
 import cn.entertech.device.api.IDeviceType
 
-object SkinDevice: IDeviceType
+object SkinDevice: IDeviceType{
+    override fun getDeviceType(): Int {
+        return -1
+    }
+
+    override fun getDeviceTypeName(): String {
+        return "SkinDevice"
+    }
+}
