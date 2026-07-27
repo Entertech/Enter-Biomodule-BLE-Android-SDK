038eac6d5acf492c17709d5f11bfc5710490850d
wk <1226426603@qq.com>
2026-07-27 10:51:49 +0800

    3071（3.0.7.1）

--- demo/build.gradle.kts
+++ demo/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "cn.entertech.flowtimeble"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3070
-        versionName = "3.0.7.0"
+        versionCode = 3071
+        versionName = "3.0.7.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
